2a33efb105a9c26374b93c36164c7ad2fa08048a
CesarErives <a6519150012@utch.edu.mx>
2021-05-29 02:19:14 -0600

    typo fixed

--- db_config.py
+++ db_config.py
@@ -8,5 +8,5 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME= os.getenv("DB_NAME")
 
-client = pymongo.MongoClient("mongodb+srv://{DB_USER}:{DB_PASSWORD}@testcluster.gpfjy.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
-db = client.test
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@testcluster.gpfjy.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+db = client.db_example
