73fedb23c8df041d92ca5cb5b0bd60e1ee76a0e3
CesarErives <a6519150012@utch.edu.mx>
2021-05-31 22:20:17 -0600

    Added get,put and abort if not exist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, app, jsonify,request
+from flask import Flask, app, json, jsonify,request
+from flask.wrappers import Response
 from flask_restful import Api,Resource,reqparse,abort
 from flask_pymongo import pymongo
+from bson.json_util import dumps,ObjectId
 import db_config as database
 
 app=Flask(__name__)
@@ -11,8 +13,15 @@ class Test(Resource):
         return jsonify({"message":"Connected"})
 
 class Badge(Resource):
+
+    def get(self,by,data):
+        response = self.abort_if_not_exist(by,data)
+        response['_id'] = str(response['_id'])
+        return jsonify(response)
+
+
     def post(self):
-        database.db.Badge.insert_one({
+        _id = str(database.db.Badge.insert_one({
             'HeaderBackground': request.json['HeaderBackground'],
             'ProfilePic':request.json['ProfilePic'],
             'name':request.json['name'],
@@ -21,14 +30,46 @@ class Badge(Resource):
             'games':request.json['games'],
             'achievements':request.json['achievements'],
             'followers':request.json['followers']
-        })
+        }).inserted_id)
+        return jsonify({"_id":_id})
+
+    def put(self,by,data):
+        response=self.abort_if_not_exist(by,data)
+
+        for key,value in request.json.items():
+            response[key] = value
+        
+        database.db.Badge.update_one({'_id':ObjectId(response['_id'])}, 
+        {'$set':{
+            'HeaderBackground': response['HeaderBackground'],
+            'ProfilePic':response['ProfilePic'],
+            'name':response['name'],
+            'age':response['age'],
+            'city':response['city'],
+            'games':response['games'],
+            'achievements':response['achievements'],
+            'followers':response['followers']
+        }})
+        response['_id'] = str(response['_id'])
+        return jsonify(response)
+
+    def abort_if_not_exist(self,by,data):
+        if by == "_id":
+            response = database.db.Badge.find_one({"_id":ObjectId(data)})
+        else:
+            response = database.db.Badge.find_one({f"{by}":data})
+
+        if response:
+            return response
+        else:
+            abort(jsonify({"Status":404, f"{by}":f"{data} not found"}))
 
 class AllBadge(Resource):
     """Get all badges"""
     def get(self):
         pass
 
-api.add_resource(Badge,'/new/')
+api.add_resource(Badge,'/new/','/<string:by>=<string:data>/')
 api.add_resource(Test,'/test/')
 
 
