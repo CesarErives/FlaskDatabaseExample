2672acf5edffc09a7b77e77abff7cad2ca923d81
CesarErives <a6519150012@utch.edu.mx>
2021-06-03 23:01:45 -0600

    Added some different queries

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import re
 from flask import Flask, app, json, jsonify,request
 from flask.wrappers import Response
 from flask_restful import Api
 from flask_pymongo import pymongo
 from bson.json_util import dumps,ObjectId
+from werkzeug.wrappers import response
 import db_config as database
 
 #Resources
@@ -12,7 +14,37 @@ from res.badges import Badges
 app=Flask(__name__)
 api=Api(app)
 
+
+@app.route('/all/adults/')
+def get_adults():
+    response = list (database.db.Badge.find({'age':{"$gte": "21"}}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+@app.route('/all/projection/')
+def get_name_and_age():
+    response = list(database.db.Badge.find({'age':{"$gte": "21"}}, {'name':1,'age':1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+@app.route('/all/kids/')
+def get_kids():
+    response = list(database.db.Badge.find({'age':{"$lte": "21"}}, {'name':1,'age':1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+
 api.add_resource(Badge,'/new/','/<string:by>=<string:data>/')
 api.add_resource(Badges,'/all/', '/delete/all/')
+
 if __name__ == '__main__':
     app.run(load_dotenv=True)
