ca129d2c3196b2e3308c28f9313d29687f6fc3af
CesarErives <a6519150012@utch.edu.mx>
2021-06-03 22:19:09 -0600

    Added get function and delete function

--- res/badges.py
+++ res/badges.py
@@ -1,4 +1,19 @@
-class AllBadge(Resource):
+from flask import json, jsonify,request
+from flask_restful import Resource,abort
+from flask_pymongo import pymongo
+from bson.json_util import ObjectId
+from werkzeug.wrappers import response
+import db_config as database
+
+class Badges(Resource):
     """Get all badges"""
     def get(self):
-        pass
+        response = list(database.db.Badge.find())
+
+        for doc in response:
+            doc['id'] = str(doc['_id'])
+
+        return jsonify(response)
+
+    def delete(self):
+        return database.db.Badge.delete_many({}).deleted_count
