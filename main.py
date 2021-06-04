8271e527b667f1e0d2b1c05bbe950052c9db6401
CesarErives <a6519150012@utch.edu.mx>
2021-06-03 22:18:10 -0600

    deleted unnecessary imports and changed directory

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from flask import Flask, app, json, jsonify,request
 from flask.wrappers import Response
-from flask_restful import Api,Resource,reqparse,abort
+from flask_restful import Api
 from flask_pymongo import pymongo
 from bson.json_util import dumps,ObjectId
 import db_config as database
 
 #Resources
 from res.badge import Badge
-
+from res.badges import Badges
 
 app=Flask(__name__)
 api=Api(app)
 
 api.add_resource(Badge,'/new/','/<string:by>=<string:data>/')
-api.add_resource(Test,'/test/')
-
-
+api.add_resource(Badges,'/all/', '/delete/all/')
 if __name__ == '__main__':
     app.run(load_dotenv=True)
