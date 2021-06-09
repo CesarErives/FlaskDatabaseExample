e4e7dc8ce5725edd4354e2324a2032c645ee624d
CesarErives <a6519150012@utch.edu.mx>
2021-06-09 08:30:53 -0600

    Created posts

---
+++ res/posts.py
@@ -0,0 +1,49 @@
+from os import name
+from flask import Flask, json, jsonify,request
+from flask_restful import Resource, abort
+from flask_pymongo import pymongo
+from bson.json_util import ObjectId
+from pymongo.common import RETRY_READS
+from werkzeug.wrappers import response
+import db_config as database
+
+class Posts(Resource):
+
+    def get(self,_id):
+        response = self.abort_if_not_exist(_id)
+        response['_id'] = str(response['_id'])
+        return jsonify(response)
+
+    def post(self,_id):
+        response = self.abort_if_not_exist(_id)
+        database.db.Badge.update_one({"_id": ObjectId(_id)},{"$push": {
+            "posts":{
+                "id":request.json["id"],
+                "name":request.json["name"],
+                "img":request.json["img"],
+                "date":request.json["date"]
+
+            }
+        }})
+
+        return jsonify({"Message": f"The post {request.json} was successfully created"})
+
+    def put(self,_id,uuid):
+        response = self.abort_if_not_exist(_id)
+        database.db.Badge.update_one({"_id":ObjectId(_id),"posts.id": uuid},
+        {"$set":{
+            "posts.$.name":request.json["name"],
+            "posts.$.img":request.json["img"],
+            "posts.$.date":request.json["date"]
+        }})
+
+        return jsonify(request.json)
+
+    def abort_if_not_exist(self,_id):
+        response = database.db.Badge.find_one({'_id':ObjectId},{"name":1, "posts":1})
+
+        if response:
+            return response
+
+        else:
+            abort(jsonify({"Status":404, "_id": f"{_id} not found"}))
